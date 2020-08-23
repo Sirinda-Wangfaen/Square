e944808ba233120176a1d66a16006cfc5477b2e7
Sirinda <69206649+Sirinda-Wangfaen@users.noreply.github.com>
2020-08-23 07:27:35 +0700

    Add project files.

---
+++ Square/Source.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int x, i, j;
+	printf("Enter number : ");
+	scanf("%d", &x);
+	for (i = 0; i < x; i++)
+	{
+		for (j = 0; j < x; j++)
+			if (i == 0 || i == x - 1 || j == 0 || j == x - 1)
+				printf("*");
+			else
+				printf(" ");
+		printf("\n");
+	}
+	return 0;
+}
